b9d15d171a7d22b2b424bfedae0b49a557ca00c6
Valery Bashkatov <valery@bashkatov.org>
2016-12-25 23:27:00 +0300

    0.0.6: Fixed error descriptions.

--- Sources/RSAError.swift
+++ Sources/RSAError.swift
@@ -15,25 +15,26 @@ public struct RSAError: Error, CustomStringConvertible {
     
     // MARK: - Properties
     
+    /// The error codes descriptions.
+    private static let descriptions = [
+        -4: "The function or operation is not implemented",
+        -50: "One or more parameters passed to a function were not valid",
+        -108: "Failed to allocate memory",
+        -25291: "No keychain is available",
+        -25299: "An item with the same primary key attributes already exists",
+        -25300: "The item cannot be found",
+        -25308: "Interaction with the user is required in order to grant access or process a request; however, user interaction with the Security Server has been disabled by the program",
+        -26275: "Unable to decode the provided data",
+        -34018: "Internal error when a required entitlement isn't present. Keychain entitlement required",
+        10: "Invalid digest algorithm. Available values: PKCS1SHA1, PKCS1SHA224, PKCS1SHA256, PKCS1SHA384 or PKCS1SHA512"
+    ]
+    
     /// The error code.
     public let code: Int
     
     /// The description.
     public var description: String {
-        let descriptions = [
-            -4: "The function or operation is not implemented",
-            -50: "One or more parameters passed to a function were not valid",
-            -108: "Failed to allocate memory",
-            -25291: "No keychain is available",
-            -25299: "An item with the same primary key attributes already exists",
-            -25300: "The item cannot be found",
-            -25308: "Interaction with the user is required in order to grant access or process a request; however, user interaction with the Security Server has been disabled by the program",
-            -26275: "Unable to decode the provided data",
-            -34018: "Internal error when a required entitlement isn't present. Keychain entitlement required",
-            10: "Invalid digest algorithm. Available values: PKCS1SHA1, PKCS1SHA224, PKCS1SHA256, PKCS1SHA384 or PKCS1SHA512"
-        ]
-        
-        return descriptions[code] ?? "Unknown error (\(code))"
+        return RSAError.descriptions[code] ?? "Unknown error (\(code))"
     }
     
     // MARK: - Initialization
